472a73b25468f6cafd55b670deed31af2bbd6eb7
Rahul Dey <rahuldey9623@gmail.com>
2025-05-20 09:22:34 +0000

    Added api key credits

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Header
 import ollama
 import os
 from dotenv import load_dotenv
+from http import HTTPStatus
 
 load_dotenv()
 
 ollamaServer = os.getenv("OLLAMA_SERVER_URL") or ""
 model = os.getenv("OLLAMA_MODEL_ID") or ""
 
+API_KEY_CREDITS = {os.getenv("API_KEY"): 3}
+
 app = FastAPI()
 client = ollama.Client(ollamaServer)
 
+
+def verify_Api_key(x_api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(x_api_key, 0)
+
+    if credits <= 0:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="No credits left for this api key. Please add credits before query.")
+    
+    return x_api_key
+
 @app.post("/generate")
-def generate(prompt: str):
+def generate(prompt: str, x_api_key: str = Depends(verify_Api_key)):
+    API_KEY_CREDITS[x_api_key] -= 1
     response = client.chat(model=model, messages=[{"role": "user", "content": prompt}])
     return {"response": response["message"]["content"]}
