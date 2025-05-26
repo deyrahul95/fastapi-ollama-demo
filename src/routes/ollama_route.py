fd75fa40e0e67cb39318e9cbb2f44f141488752a
Rahul Dey <rahuldey9623@gmail.com>
2025-05-26 05:00:14 +0000

    Update DI setup

--- src/routes/ollama_route.py
+++ src/routes/ollama_route.py
@@ -9,7 +9,14 @@ from src.services.ollama_service import OllamaService
 
 router = APIRouter(prefix="/ollama", tags=["Ollama"])
 
-ollama_service = OllamaService(model_url=OLLAMA_SERVER_URL, model_id=MODEL_ID)
+# Get ollama service
+def get_ollama_service() -> OllamaService:
+    return OllamaService(model_url=OLLAMA_SERVER_URL, model_id=MODEL_ID)
+
+
+# Get credit service
+def get_credit_Service() -> CreditService:
+    return CreditService()
 
 
 @router.post("/generate", status_code=HTTPStatus.OK)
@@ -17,13 +24,14 @@ def generate(
     prompt: str,
     _=Depends(verify_Api_key),
     ip: str = Depends(get_client_ip),
-    creditService: CreditService = Depends(CreditService),
+    credit_service: CreditService = Depends(get_credit_Service),
+    ollama_service: OllamaService = Depends(get_ollama_service),
 ):
-    creditService.verify_credits(ip=ip)
+    credit_service.verify_credits(ip=ip)
 
     response = ollama_service.chat(query=prompt)
     tokens = len(response.split(" "))
-    creditService.credit_used(ip=ip, tokens=tokens)
+    credit_service.credit_used(ip=ip, tokens=tokens)
 
     return {"response": response}
 
@@ -32,8 +40,8 @@ def generate(
 async def get_credits(
     _=Depends(verify_Api_key),
     ip: str = Depends(get_client_ip),
-    authService: CreditService = Depends(CreditService),
+    credit_service: CreditService = Depends(get_credit_Service),
 ):
-    authService.reset_credits(ip=ip)
+    credit_service.reset_credits(ip=ip)
 
-    return {"remaining_credits": round(authService.get_remaining_credits(ip=ip), 2)}
+    return {"remaining_credits": round(credit_service.get_remaining_credits(ip=ip), 2)}
